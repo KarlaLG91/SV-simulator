a0dfd94e37b3697852df052e6c5884a118b91b6d
KarlaLG91 <klg1219sh@gmail.com>
2018-05-09 15:04:35 -0500

    Modifying R script for proper use of SV simulation

--- RSVSim.R
+++ RSVSim.R
@@ -1,6 +1,8 @@
 library(RSVSim)
-#Genome <- (file= "/home/karla/SV-simulator/test/reference/human_g1k_v37.fasta.gz")
-#ReapeatMasker <- (file= "")
-simulateSV(output=".", genome=Genome, dels=10, ins=10, inv=10, dups=10, trans=10,
-           sizeDels=10000, sizeIns=10000, sizeInvs=10000, sizeDups=10000,
-           repeatBias=FALSE, bpFlankSize=50, percSNPs=0.25, indelProb=0.5, maxIndelSize=10)
+library(RSVSim)
+simulateSV(output="/media/genomics/disco1/klozano.bkp/results/", \
+           genome="/media/genomcs/disco1/klozano.bkp/reference/hg19/hg19.fa" \
+           chr=c("chr21", "chr22"), dels=1, ins=1, invs=1, dups=1, \
+           trans=1, size=1000, maxDups=10, percCopiedIns=0, \
+           percBalancedTrans=1, repeatBias=FALSE, bpSeqSize=150, \
+           random=TRUE, seed=666, verbose=TRUE)
